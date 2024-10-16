367ab427aa86e44a3e4073fc7c41da4ebdc78d29
arbisyarifudin@gmail.com <arbisyarifudin@gmail.com>
2024-10-16 17:22:01 +0700

    add satuin-plugin/css/styles.css and update satuin-plugin/js/satuin-elementor-form-script

--- js/satuin-elementor-form-script.js
+++ js/satuin-elementor-form-script.js
@@ -1,3 +1,3 @@
-jQuery(document).ready(function($) {
-    console.log('Satuin Elementor Form script loaded');
-});
+// jQuery(document).ready(function($) {
+//     console.log('Satuin Elementor Form script loaded');
+// });
